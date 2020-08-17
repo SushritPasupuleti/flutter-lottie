a09df71dce66842ff759a4cb784630e66452acd6
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-17 10:41:49 +0530

    Create onboard.dart

---
+++ flutter_lottie/lib/screens/onboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class OnBoard extends StatefulWidget {
+  OnBoard({Key key}) : super(key: key);
+
+  @override
+  _OnBoardState createState() => _OnBoardState();
+}
+
+class _OnBoardState extends State<OnBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}

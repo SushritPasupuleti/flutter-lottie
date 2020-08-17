95f2aeeaf68688cfcc9355743a63f584c320fa16
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-17 10:42:43 +0530

    Update onboard.dart

--- flutter_lottie/lib/screens/onboard.dart
+++ flutter_lottie/lib/screens/onboard.dart
@@ -8,11 +8,25 @@ class OnBoard extends StatefulWidget {
   _OnBoardState createState() => _OnBoardState();
 }
 
-class _OnBoardState extends State<OnBoard> {
+class _OnBoardState extends State<OnBoard> with TickerProviderStateMixin {
+  AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: child,
+      child: child,
     );
   }
 }
